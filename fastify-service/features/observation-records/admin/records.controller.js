cf2cb8fdc27cd9c3250e6f6a3a5a34e5eb26274d
haikalardzi <haikalardzi@gmail.com>
2025-06-24 15:19:05 +0700

    update: add file url on record data

--- fastify-service/features/observation-records/admin/records.controller.js
+++ fastify-service/features/observation-records/admin/records.controller.js
@@ -1,3 +1,4 @@
+import { FASTIFY_URL } from '../../../../helper/constant.js';
 import { deleteRecord, getAllRecords } from './records.service.js';
 
 /**
@@ -18,6 +19,9 @@ export async function deleteRecordHandler(req, reply) {
 export async function fetchAllRecordsHandler(req, reply) {
     try {
         const data = await getAllRecords();
+        for (let i = 0; i < data.length; i++) {
+            data[i].url = `${FASTIFY_URL}/files/${data[i].username}/${data[i].filename}`;
+        }
         reply.send(data);
     } catch (err) {
         reply.code(500).send({ error: err.message });
