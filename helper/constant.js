187d031469d68fce629dfd216d5569690805db94
haikalardzi <>
2025-07-27 18:28:57 +0700

    fix: cors error

--- helper/constant.js
+++ helper/constant.js
@@ -13,4 +13,4 @@ export const FASTIFY_URL = `http://${process.env.SERVER_HOST}:${process.env.SERV
 
 export const WS_URL = `ws://${process.env.WS_HOST}:${process.env.WS_PORT}` || 'ws://localhost:9005';
 
-export const FE_URL = 'http://proto.28manzilah.com';
+export const FE_URL = 'https://proto.28manzilah.com';
